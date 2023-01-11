af6e4fcacc0f6913bb088da0dae5963cfba84162
LeweC <lewe22lewe@googlemail.com>
2023-01-11 13:35:16 +0100

    inital commit

---
+++ translations/translation_de_DE.ts
@@ -0,0 +1,265 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ich denke mal, ihr habt alles verstanden? So schwer ist das ja nicht! Einfaches Schnick, Schnack, Schnuck.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ich denke mal, ihr habt alles verstanden? So schwer ist das ja nicht! Einfaches Schnick, Schnack, Schnuck.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Also dann: lasst uns anfangen!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Also dann: lasst uns anfangen!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Du möchtest gegen mich spielen? Dann wähle bitte eine Karte.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Du möchtest gegen mich spielen? Dann wähle bitte eine Karte.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hast du eine ausgewählt? Gut. Nun dann:</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hast du eine ausgewählt? Gut. Nun dann:</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/Animated Say (14)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Such dir nun am besten einen anderen Gegner oder eine andere Gegnerin, damit ich gegen die nächste Person spielen kann.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Such dir nun am besten einen anderen Gegner oder eine andere Gegnerin, damit ich gegen die nächste Person spielen kann.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Möchtest du die Auflockerungsübung wirklich beenden?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Möchtest du die Auflockerungsübung wirklich beenden?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ok, schade. Das hat Spaß gemacht. Dann sehen wir uns beim nächsten Mal wieder. Tschau!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ok, schade. Das hat Spaß gemacht. Dann sehen wir uns beim nächsten Mal wieder. Tschau!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ok, dann lass uns weiter spielen.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ok, dann lass uns weiter spielen.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (10)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Mein Papier gegen dein Papier.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Mein Papier gegen dein Papier.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (11)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ich gewinne. Juhu</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ich gewinne. Juhu</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (12)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Unentschieden.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Unentschieden.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (13)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Du gewinnst. Herzlichen Glückwunsch!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Du gewinnst. Herzlichen Glückwunsch!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Meine Schere gegen deine Schere.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Meine Schere gegen deine Schere.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Meine Schere gegen deinen Stein.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Meine Schere gegen deinen Stein.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Meine Schere gegen dein Papier.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Meine Schere gegen dein Papier.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Mein Stein gegen deine Schere.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Mein Stein gegen deine Schere.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Mein Stein gegen deinen Stein.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Mein Stein gegen deinen Stein.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Mein Stein gegen dein Papier.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Mein Stein gegen dein Papier.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (8)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Mein Papier gegen deine Schere.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Mein Papier gegen deine Schere.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Animated Say (9)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Mein Papier gegen deinen Stein.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Mein Papier gegen deinen Stein.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Papier/behavior_layer1/keyframe30/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnick</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnick</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Papier/behavior_layer1/keyframe30_copy/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnack</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnack</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Papier/behavior_layer1/keyframe30_copy_copy/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnuck</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnuck</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Schere/behavior_layer1/keyframe30/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnick</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnick</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Schere/behavior_layer1/keyframe30_copy/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnack</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnack</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Schere/behavior_layer1/keyframe30_copy_copy/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnuck</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnuck</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Stein/behavior_layer1/keyframe30/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnick</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnick</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Stein/behavior_layer1/keyframe30_copy/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnack</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnack</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/game/logic/Stein/behavior_layer1/keyframe30_copy_copy/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Schnuck</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Schnuck</translation>
+        </message>
+    </context>
+</TS>
